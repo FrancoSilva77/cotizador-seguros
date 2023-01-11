5be1e35ad6e07d2d1af59efcba3a54d3b7dc87fc
Franco Silva <franco_tigres18@hotmail.com>
2023-01-11 09:26:26 -0600

    Mostrar total add

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -13,6 +13,7 @@ const CotizadorProvider = ({ children }) => {
 
   const [error, setError] = useState('')
   const [resultado, setResultado] = useState(0)
+  const [cargando, setCargando] = useState(false)
 
   const handleChangeDatos = e => {
     setDatos({
@@ -42,8 +43,12 @@ const CotizadorProvider = ({ children }) => {
     // Formatear dinero
     resultado = formatearDinero(resultado)
 
-    setResultado(resultado)
+    setCargando(true)
 
+    setTimeout(() => {
+      setCargando(false)
+      setResultado(resultado)
+    }, 3000);
   }
 
   return (
@@ -53,7 +58,9 @@ const CotizadorProvider = ({ children }) => {
         handleChangeDatos,
         error,
         setError,
-        cotizarSeguro
+        cotizarSeguro,
+        resultado, 
+        cargando
       }}
     >
       {children}
